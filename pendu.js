7fc419c4f764527e29625710adfc469904d1ccda
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-02 17:01:17 +0200

    end

--- pendu.js
+++ pendu.js
@@ -5,24 +5,34 @@ const { readFileSync } = require('fs')
 
 let dictionary = readFileSync('dict.txt', 'utf-8').split('\n')
 let answer = dictionary[randomInt(0, dictionary.length)]
-let finded = '_ '.repeat(answer.length)
-let nbtry = 12
+let finded = Array(answer.length).fill('_')
+let nbtry = 10
+let history = []
 
 while (true) {
-  let letter = redlineSync.question(finded + '> ').toUpperCase()
+  let letter = redlineSync.question('Decypher this word : ' + finded.join(' ') + ' > ').toLowerCase()
+  if (letter.length !== 1) { console.log('Pick only one letter !'); continue }
+  history.push(letter)
+  if (history.includes(letter)) { console.log('You already try this letter !'); continue }
+
   if (answer.includes(letter)) {
     console.log(chalk.green('You have find a new letter !!!'))
-    finded = finded.replace('_', letter)
+    for (let i = 0; i < answer.length; i++) {
+      if (answer[i] === letter) {
+        finded[i] = letter
+      }
+    }
   }
   else {
     console.log(chalk.red('You find nothing this time, try again.'))
     nbtry--
   }
+
   if (!nbtry) {
     console.log(chalk.magenta('The boy was been hanged, you lost the game!'))
     process.exit(0)
   }
-  if (finded === answer) {
+  if (!finded.includes('_')) {
     console.log(chalk.cyan('Congrats, the boy still alive, you won the game!'))
     process.exit(0)
   }
