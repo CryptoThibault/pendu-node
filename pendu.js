36d4ebb02caaa141a6b428d820acc1794ab686b0
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-02 18:36:10 +0200

    end

--- pendu.js
+++ pendu.js
@@ -10,11 +10,11 @@ let history = []
 let lignRender = 7
 let nbtry = readFileSync('render.txt', 'utf-8').split('\n').length / lignRender
 
-
 console.log('\nBienvenue dans le jeu du pendu !\n\n')
 while (true) {
   let Visual = readFileSync('render.txt', 'utf-8').split('\n').slice(7 * nbtry - lignRender, lignRender * nbtry).join('\n')
   console.log(chalk.yellow(`Il vous reste ${nbtry} essaies\n`) + Visual)
+
   let letter = redlineSync.question('Trouver ce mot : ' + finded.join(' ') + ' > ').toLowerCase()
   if (letter.length !== 1) { console.log('Choisi seulement une lettre !'); continue }
   if (history.includes(letter)) { console.log('Choisi une lettre pas encore demandé !'); continue }
@@ -32,7 +32,6 @@ while (true) {
     console.log(chalk.red('Vous avez rien trouver cette fois, essayez encore.'))
     nbtry--
   }
-
   if (!nbtry) {
     console.log(chalk.magenta('Malheuresement, le garçon a été pendu, vous avez perdu.'))
     process.exit(0)
@@ -42,4 +41,3 @@ while (true) {
     process.exit(0)
   }
 }
-
