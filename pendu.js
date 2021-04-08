b76540ec3921dec1771cebefa2fccd2b2ff22865
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-08 11:54:59 +0200

    end

--- pendu.js
+++ pendu.js
@@ -3,49 +3,59 @@ const redlineSync = require('readline-sync')
 const { randomInt } = require('crypto')
 const { readFileSync, writeFileSync } = require('fs')
 
+let lignRender = 7
 let dictionary = readFileSync('dict.txt', 'utf-8').split('\n')
 let answer = dictionary[randomInt(0, dictionary.length)]
 let finded = Array(answer.length).fill('_')
 let history = []
-let lignRender = 7
 let nbtry = readFileSync('render.txt', 'utf-8').split('\n').length / lignRender
+let scorestab = JSON.parse(readFileSync('scores.json', 'utf-8')).scores
 
-console.log('\nBienvenue dans le jeu du pendu !\n\n')
-let name = redlineSync.question('Entrer votre nom : ')
-while (true) {
-  let visual = readFileSync('render.txt', 'utf-8').split('\n').slice(lignRender * nbtry - lignRender, lignRender * nbtry).join('\n')
-  console.log(chalk.yellow(`Il vous reste ${nbtry} essaies\n`) + visual)
+// Début du jeu
+do {
+  console.log('\nBienvenue dans le jeu du pendu !\n\n')
+  let name = redlineSync.question('Entrer votre nom : ')
+  // Début d'un essai
+  while (true) {
+    let visual = readFileSync('render.txt', 'utf-8').split('\n').slice(lignRender * nbtry - lignRender, lignRender * nbtry).join('\n')
+    console.log(chalk.yellow(`Il vous reste ${nbtry} essaies\n`) + visual)
+    let letter = redlineSync.question('Trouver ce mot : ' + finded.join(' ') + ' > ').toLowerCase()
+    if (letter.length !== 1) { console.log('Choisi seulement une lettre !'); continue }
+    if (history.includes(letter)) { console.log('Choisi une lettre pas encore demandé !'); continue }
+    history.push(letter)
 
-  let letter = redlineSync.question('Trouver ce mot : ' + finded.join(' ') + ' > ').toLowerCase()
-  if (letter.length !== 1) { console.log('Choisi seulement une lettre !'); continue }
-  if (history.includes(letter)) { console.log('Choisi une lettre pas encore demandé !'); continue }
-  history.push(letter)
-
-  if (answer.includes(letter)) {
-    console.log(chalk.green('Vous avez trouver une lettre !!!'))
-    for (let i = 0; i < answer.length; i++) {
-      if (answer[i] === letter) {
-        finded[i] = letter
+    // Lettre inclu dans la réponse
+    if (answer.includes(letter)) {
+      console.log(chalk.green('Vous avez trouver une lettre !!!'))
+      for (let i = 0; i < answer.length; i++) {
+        // Lettre similaire à une lettre de la réponse 
+        if (answer[i] === letter) { finded[i] = letter }
       }
     }
-  }
-  else {
-    console.log(chalk.red('Vous avez rien trouver cette fois, essayez encore.'))
-    nbtry--
-  }
-  if (!nbtry) {
-    console.log(chalk.magenta('Malheuresement, le garçon a été pendu, vous avez perdu.'))
-    process.exit(0)
-  }
-  if (!finded.includes('_')) {
-    console.log(chalk.cyan('Bravo le garçon est toujours en vie, vous remporter la partie !'))
-    let scorestab = JSON.parse(readFileSync('scores.json', 'utf-8')).scores
-    scorestab.push({ name: name, score: nbtry })
-    writeFileSync('scores.json', JSON.stringify({ scores: scorestab }))
-    console.log('Scores : \n')
-    for (let i = 0; i < scorestab.length; i++) {
-      console.log(`name: ${scorestab[i].name}   score: ${scorestab[i].score}`)
+    else {
+      console.log(chalk.red('Vous avez rien trouver cette fois, essayez encore.'))
+      nbtry--
     }
-    process.exit(0)
+
+    // Nombre d'essai égal à 0
+    if (!nbtry) {
+      console.log(chalk.magenta('Malheuresement, le garçon a été pendu, vous avez perdu.'))
+      console.log('Scores : \n')
+      for (let i = 0; i < scorestab.length; i++) {
+        console.log(`name: ${scorestab[i].name}   score: ${scorestab[i].score}`)
+      }
+      process.exit(0)
+    }
+    // Mot à trouvé ne continent plus d'underscore
+    if (!finded.includes('_')) {
+      console.log(chalk.cyan('Bravo le garçon est toujours en vie, vous remporter la partie !'))
+      scorestab.push({ name: name, score: nbtry, word: answer })
+      writeFileSync('scores.json', JSON.stringify({ scores: scorestab }))
+      console.log('Scores : \n')
+      for (let i = 0; i < scorestab.length; i++) {
+        console.log(`nom: ${scorestab[i].name}   score: ${scorestab[i].score}  mot: ${scorestab[i].word}`)
+      }
+      process.exit(0)
+    }
   }
-}
+} while (redlineSync.keyInYN('Voulez vous rejouez ?'))
